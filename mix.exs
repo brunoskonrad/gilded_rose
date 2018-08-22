71517d93503471000fe2926e70a9b816e094a58e
Bruno Konrad <bruno.skon@gmail.com>
2018-08-23 00:00:31 +0200

    Add ExCoveralls dep to calculate test coverage

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule GildedRose.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -23,6 +30,7 @@ defmodule GildedRose.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:excoveralls, "~> 0.9", only: :test}
     ]
   end
 end
