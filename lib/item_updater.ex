73d2ab37f14c329d6d5095e5c2e8c1e1dbf30d58
Bruno Konrad <bruno.skon@gmail.com>
2018-08-26 13:00:36 +0200

    Change function declaration from block to line

--- lib/item_updater.ex
+++ lib/item_updater.ex
@@ -1,21 +1,7 @@
 defmodule ItemUpdater do
-  def get("Sulfuras") do
-    Item.Sulfuras
-  end
-
-  def get("Aged Brie") do
-    Item.AgedBrie
-  end
-
-  def get("Backstage passes") do
-    Item.BackstagePasses
-  end
-
-  def get("Conjured") do
-    Item.Conjured
-  end
-
-  def get(_name) do
-    Item.Regular
-  end
+  def get("Sulfuras"), do: Item.Sulfuras
+  def get("Aged Brie"), do: Item.AgedBrie
+  def get("Backstage passes"), do: Item.BackstagePasses
+  def get("Conjured"), do: Item.Conjured
+  def get(_name), do: Item.Regular
 end
